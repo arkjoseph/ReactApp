0414720e830e141fd65643e8bc70fbd082995d82
josephgarrido <josephpgarrido@gmail.com>
2017-09-14 10:54:05 -0700

    routing updated

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 import App from './App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
-import CoursesPage from './components/course/FyRatesPage';
+import FyRatesPage from './components/course/FyRatesPage';
 import ManageCoursesPage from './components/course/ManageFyRatePage';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
-        <Route path="courses" component={CoursesPage}/>
+        <Route path="fy-rates" component={FyRatesPage}/>
         <Route path="course" component={ManageCoursesPage}/>
         <Route path="course/:id" component={ManageCoursesPage}/>
         <Route path="about" component={AboutPage}/>
